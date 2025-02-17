c0872236f4a02428e2a240f474da60b293b27db8
Omkar Wakchaure <omkarwakchaure2019@gmail.com>
2025-02-17 12:10:48 +0530

    user and video model done

--- src/index.js
+++ src/index.js
@@ -12,25 +12,7 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
+    console.log("MONGO DB connection failed !!!", err);
   });
 
-/*
-import express from "express";
-const app = express();
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERR", error);
-      throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
+
