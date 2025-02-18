4091c6ab192de6e3ab098c308c1723ef56d9c329
Omkar Wakchaure <omkarwakchaure2019@gmail.com>
2025-02-18 16:16:22 +0530

    login and logout done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
@@ -12,4 +16,6 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
