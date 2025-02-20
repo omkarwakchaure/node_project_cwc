f50148b9679ee283b40db58fb5fa39b988151a56
Omkar Wakchaure <omkarwakchaure2019@gmail.com>
2025-02-20 12:20:04 +0530

    fix: update logoutUser to unset refreshToken and improve cookie handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -154,8 +154,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
@@ -166,12 +166,14 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "None",
+    path: "/",
   };
-
+  res.clearCookie("accessToken", options);
+  res.clearCookie("refreshToken", options);
   return res
     .status(200)
-    .clearCookie("accessToken", options)
-    .clearCookie("refreshToken", options)
+
     .json(new ApiResponse(200, "User logged out successfully"));
 });
 
@@ -442,7 +444,6 @@ const getWatchHistory = asyncHandler(async (req, res) => {
       )
     );
 });
-
 
 export {
   registerUser,
